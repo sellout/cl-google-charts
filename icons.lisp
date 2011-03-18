c89d508460574c15b0d29e0392be8dba189e1ed2
Greg Pfeil <greg@technomadic.org>
2011-03-17 22:17:07 -0400

    Implemented bubble icons.

--- icons.lisp
+++ icons.lisp
@@ -1,5 +1,33 @@
 (in-package #:google-charts)
 
+(defun symbol->string (symbol)
+  (substitute #\_ #\- (string-downcase symbol)))
+
 (defclass icon ()
   (icon-type
    data))
+
+(defclass bubble (icon)
+  ((icon :initform nil :initarg :icon :accessor icon)
+   (text :initarg :text :accessor text) ;; string for text, list for texts
+   (bigp :initform nil :initarg :bigp :accessor bigp)
+   (shadowp :initform nil :initarg :shadowp :accessor shadowp)
+   (frame-style :initarg :frame-style :accessor frame-style)
+   (fill-color :initarg :fill-color :accessor fill-color)
+   (text-color :initarg :text-color :accessor text-color)))
+
+(defmethod get-parameters append ((chart bubble))
+  `(("chst" . ,(format nil
+                       "d_bubble~:[~;_icon~]_text~:[~;s~]_~:[small~;big~]~:[~;_withshadow~]"
+                       (icon chart)
+                       (listp (text chart))
+                       (or (listp (text chart))
+                           (and (icon chart) (bigp chart)))
+                       (shadowp chart)))
+    ("chld" . ,(format nil "~@[~a|~]~a|~@[~a|~]~a|~a~{|~a~}"
+                       (when (icon chart) (symbol->string (icon chart)))
+                       (frame-style chart)
+                       (when (stringp (text chart)) (text chart))
+                       (fill-color chart)
+                       (text-color chart)
+                       (when (listp (text chart)) (text chart))))))
--- package.lisp
+++ package.lisp
@@ -2,11 +2,13 @@
   (:use #:cl)
   (:export #:uri #:image-data #:image-map #:validate
            ;; charts
-           #:bar-chart #:line-chart  #:meter #:pie-chart #:radar-chart
+           #:bar-chart #:line-chart #:meter #:pie-chart #:radar-chart
            #:scatter-plot #:venn-diagram
+           ;; icons
+           #:bubble
            ;; other image types
            #:formula #:graph #:map #:qr-code
            ;; elements
            #:axis #:legend #:series
-           ;; abstract charts
-           #:chart))
+           ;; abstract classes
+           #:chart #:icon))
